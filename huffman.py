351749257d7bebf6887e447bad6804a09025f71b
kaibush <kaibush@163.com>
2019-02-15 21:52:59 +0800

    umodify huffman.py

--- huffman.py
+++ huffman.py
@@ -1,56 +1,3 @@
-##def reverse_num(s):
-##    num = []
-##    while (s != 0):
-##        num.append(s % 10)
-##        s /= 10
-##    total = len(num) - 1
-##    n = 0
-##    for i in num:
-##        n += i * (10** total)
-##        total -= 1
-##    return n
-##
-##reverse_num(12355)
-##    
-##    
-##
-##for i in range(10000, 100000):
-##    if i<<2 == int(reverse_num(i)):
-##        break
-##print i
-
-
-
-##for i in range(10000, 100000):
-##    j = 0
-##    t = i
-##    if i  == 21978:
-##        print
-##    while(t != 0):
-##        j = j * 10 + t % 10
-##        t /= 10
-##        if i<<2 == j:
-##            print i
-##            break
-import math
-def get_float(num):
-     m, n = math.modf(num)
-     return n, m
-
-     
-##num = 0.75
-##
-##l = []
-##while(1):
-##    num_n, num_m = get_float(num)
-##    chengfa = num_m * 2.0
-##    num_n, num = get_float(chengfa)
-##    l.append(num_n)
-##    if num == 0:
-##        break
-##print l
-
-
 decode_code = {'00':'A',
                '01':'E',
                '100':'L',
@@ -62,7 +9,7 @@ decode_code = {'00':'A',
 encode_code = dict([(y, x) for x,y in decode_code.items()])
 print encode_code
 
-org_str = 'DOORBELL'
+init_str = 'DOORBELL'
 
 def encode_it(s):
     encode_lst = []
@@ -84,7 +31,7 @@ def decode_it(encode_str):
         if not match:
             break
     return ''.join(decode_lst)
-enstr = encode_it(org_str)
+enstr = encode_it(init_str)
 print enstr
 print decode_it(enstr)
 
